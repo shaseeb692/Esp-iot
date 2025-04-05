89d4b743c9041d837d876d0ecb69648a49c179a2
shaseeb692 <shaseeb692@gmail.com>
2025-04-05 16:42:51 +0500

    n

---
+++ api/index.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+let conn = null;
+const uri = process.env.MONGODB_URI;
+
+const deviceSchema = new mongoose.Schema({
+  deviceId: String,
+  ledStatus: Boolean,
+});
+
+let Device;
+
+async function connectDB() {
+  if (conn == null) {
+    conn = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    Device = mongoose.model("Device", deviceSchema);
+  }
+}
+
+module.exports = async (req, res) => {
+  await connectDB();
+
+  const { method, url } = req;
+
+  if (method === "POST" && url === "/register") {
+    const { deviceId } = req.body;
+    let device = await Device.findOne({ deviceId });
+    if (!device) {
+      device = new Device({ deviceId, ledStatus: false });
+      await device.save();
+    }
+    res.status(200).json({ ledStatus: device.ledStatus });
+  }
+
+  else if (method === "POST" && url === "/update-led") {
+    const { deviceId, ledStatus } = req.body;
+    const device = await Device.findOneAndUpdate(
+      { deviceId },
+      { ledStatus },
+      { new: true }
+    );
+    res.status(200).json({ ledStatus: device.ledStatus });
+  }
+
+  else if (method === "GET" && url.startsWith("/led-status")) {
+    const deviceId = url.split("/")[2];
+    const device = await Device.findOne({ deviceId });
+    res.status(200).json({ ledStatus: device ? device.ledStatus : false });
+  }
+
+  else {
+    res.status(404).json({ error: "Not Found" });
+  }
+};
