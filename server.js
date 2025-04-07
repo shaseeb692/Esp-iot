26680bbd00449256e60518bd0170991b47bb965a
shaseeb692 <shaseeb692@gmail.com>
2025-04-07 06:38:28 +0500

    Full working frontend + backend with relay/slider add and delete device page

--- server.js
+++ server.js
@@ -102,3 +102,8 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Route to serve devices.html
+app.get('/devices.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'devices.html'));
+});
